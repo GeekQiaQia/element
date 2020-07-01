4849fe2e06e5b23b43b93b2752c778a80bdb6751
喜个阳阳 <xiyangbai@outlook.com>
2020-07-01 23:40:19 +0800

    Update emitter.js

--- src/mixins/emitter.js
+++ src/mixins/emitter.js
@@ -1,19 +1,39 @@
+/**
+ * 
+ * @description:由父组件向子组件向下广播
+ * @param {type:string} componentName = [value:需要广播的组件名称] 
+ * @param {type:string} eventName = [value:需要广播的事件名] 
+ * @param {type:array} params=[value:需要传入的事件参数]
+ * */
 function broadcast(componentName, eventName, params) {
+	// 通过this.$children获取当前组件包含的所有子组件对象数组，并进行遍历查找符合条件的child进行广播；
+	// 缺点：遍历所有的child,性能损耗比较高；
   this.$children.forEach(child => {
+	  // 查找子组件对象属性为componnetName的组件名；
     var name = child.$options.componentName;
 
     if (name === componentName) {
+		// 如果是需要广播的组件componentName，则进行emit一个名字为eventName的事件；
       child.$emit.apply(child, [eventName].concat(params));
     } else {
+		// 否则进行一个递归，继续向下查找组件名为componentName的组件；
       broadcast.apply(child, [componentName, eventName].concat([params]));
     }
   });
 }
 export default {
   methods: {
+	  
+	  /**
+	   * 
+	   * @description:由子组件向父组件向上派发；emit事件的事件同broadcast
+	   * @param {type:string} componentName = [value:需要派发的组件名称] 
+	   * @param {type:string} eventName = [value:需要派发的事件名] 
+	   * @param {type:array} params=[value:需要传入的事件参数]
+	   * */
     dispatch(componentName, eventName, params) {
       var parent = this.$parent || this.$root;
-      var name = parent.$options.componentName;
+      var name = parent.$opt===s.componentName;
 
       while (parent && (!name || name !== componentName)) {
         parent = parent.$parent;
